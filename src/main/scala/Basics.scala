b8746d493c7528e2928da612d3311e124638b1b5
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-03-15 19:12:25 +0100

    Classes

--- src/main/scala/Basics.scala
+++ src/main/scala/Basics.scala
@@ -1,34 +1,35 @@
+
 object Basics {
   def main(args: Array[String]): Unit = {
-    // Expressions are computable statements:
-    1 + 1
 
-    // You can output the results of expressions using println:
-    println(1) // 1
-    println(1 + 1) // 2
-    println("Hello!") // Hello!
-    println("Hello, " + "World!") // Hello, World!
+    class Point(private var _x: Int, private var _y: Int) {
 
-    // You can name the results of expressions using the 'val' keyword:
-    val x = 1 + 1
-    println(x) // 2S
-    // Named results such as x here, are called values. Referencing a value does not recompute it.
+      private val bound = 100
 
-    // Values cannot be re-assigned:
-    // x = 3 this does not compute
+      def x: Int = _x
+      def x_=(newValue: Int): Unit = {
+        if (newValue < bound)
+          _x = newValue
+        else
+          printWarning()
+      }
 
-    // The type of value can be omitted and inferred, or it can be explicitly stated:
-    val y: Int = 1 + 1
-    // Notice how the type declaration 'Int' comes after the identifier y. You also need a ':'.
+      def y: Int = _y
+      def y_=(newValue: Int): Unit = {
+        if (newValue < bound)
+          _y = newValue
+        else
+          printWarning()
+      }
 
-    // Variables
-    // Variables are like values, except you can re-assign them. You can define a variable with the 'var' keyword.
-    var z = 1 + 1
-    z = 3
-    println(z * z) // This compiles because 'z' is declared with the 'var' keyword.
-    println(z) // 9
+      private def printWarning(): Unit =
+        println("WARNING: Out of bounds")
+    }
 
-    //As with values, the type of a variable can be omitted and inferred, or it can explicitly stated;
-    var a: Int = 1 + 1
+    val point = new Point(1, 2)
+    println(point.x)
+    point.x = point.x + 1
+    println(point.x)
+
   }
 }
